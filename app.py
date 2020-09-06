eb9c4a61471d246b215c452af495fcf4cd83876d
Siddesh Pawar <49442859+sid113@users.noreply.github.com>
2020-09-07 00:11:11 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,24 +3,21 @@ import requests
 from textblob import TextBlob
 
 app = Flask(__name__)
-@app.route('/',methods=['GET'])
+@app.route('/',methods=['GET','POST'])
 def API():
-	query=request.args['Query']
+	query=request.get_json()
+	newsTitleClasses={'classes':[]}
+	for newsTitle in query['items']:
+		analyzer=TextBlob(newsTitle)
+		result=analyzer.sentiment.polarity
+		if result<0:
+			newsTitleClasses['classes'].append("Negative")
+		elif result==0:
+			newsTitleClasses['classes'].append("Neutral")
+		elif result>0 and result<=1:
+			newsTitleClasses['classes'].append("Positive")
 
-	analyzer=TextBlob(query)
-	result=analyzer.sentiment.polarity
-	if result<0:
-		return jsonify("Negative")
-	elif result==0:
-		return jsonify("Neutral")
-	elif result>0 and result<=1:
-		return jsonify("Positive")
-
-
+	return jsonify(newsTitleClasses)
 
 if __name__ == '__main__':
     app.run()
-
-
-
-
